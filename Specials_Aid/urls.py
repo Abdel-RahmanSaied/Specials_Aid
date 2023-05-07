8ee11151128fc761547b6dd4de91f98fd3ef908c
yousefelmesalamy <yousefelmesalamy@gmail.com>
2023-05-08 01:05:40 +0300

    reqs

--- Specials_Aid/urls.py
+++ Specials_Aid/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('symbols/', include("sympols.urls"), name="sympols"),
+    path('symbols/', include("sympols.urls")),
     path('auth/', include("authentications.urls")),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- sympols/urls.py
+++ sympols/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path, include
-from django.http import Http404
+
 from rest_framework.routers import DefaultRouter
 from .views import *
 
