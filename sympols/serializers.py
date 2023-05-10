cc6a796824cef0a6000591abf98dbd254179297f
Abdel-RahmanSaied <abdo.am169@gmail.com>
2023-05-10 21:02:40 +0300

    .

--- sympols/serializers.py
+++ sympols/serializers.py
@@ -3,6 +3,7 @@ from .models import *
 
 
 class Symbols_CollectoinSerializer(serializers.ModelSerializer):
+    collection_image = serializers.ImageField(max_length=None, allow_empty_file=False, use_url=True)
     class Meta:
         model = Symbols_Collectoin
         fields = ['id', 'name', 'dimension_of_symbols', 'collection_image']
